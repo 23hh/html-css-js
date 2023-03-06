c747e2b8d36a6febeff2ea8270b7886262f17d3e
23hh <lsh6166@gmail.com>
2023-03-06 21:10:35 +0900

    rab기능 수정

--- js/tab/tab.js
+++ js/tab/tab.js
@@ -1,12 +1,38 @@
-for (let i = 0; i < document.querySelectorAll(".tab-button").length; i++) {
-  document
-    .querySelectorAll(".tab-button")
-    [i].addEventListener("click", function () {
-      document
-        .querySelectorAll(".tab-button")
-        [i + 1].classList.remove("orange");
-      document.querySelectorAll(".tab-button")[i].classList.add("orange");
-      document.querySelectorAll(".tab-content")[i + 1].classList.remove("show");
-      document.querySelectorAll(".tab-content")[i].classList.add("show");
-    });
-}
+// for (let i = 0; i < document.querySelectorAll(".tab-button").length; i++) {
+document
+  .querySelectorAll(".tab-button")[0]
+  .addEventListener("click", function () {
+    document.querySelectorAll(".tab-button")[0].classList.remove("orange");
+    document.querySelectorAll(".tab-button")[1].classList.remove("orange");
+    document.querySelectorAll(".tab-button")[2].classList.remove("orange");
+    document.querySelectorAll(".tab-button")[0].classList.add("orange");
+    document.querySelectorAll(".tab-content")[0].classList.add("show");
+  });
+// }
+
+// document
+//   .querySelectorAll(".tab-button")[0]
+//   .addEventListener("click", function () {
+//     const { className } = document.querySelector("orange");
+//     alert(className);
+//   });
+// // }
+
+// const list = document.querySelectorAll(".tab-button");
+
+// for (let i = 0; i < document.querySelectorAll(".tab-button").length; i++) {
+//   .addEventListener("click", function () {
+//     const { className } = document.querySelector("orange");
+//     alert(className);
+//   });
+// }
+
+// queryselector 반복문 돌면서 click Evnet 할당
+// 해당 click event 가 일어나면 orange 인지 찾아라.
+// 오렌지가 없으면 그대로 오렌지가 있으면 remove 해라
+
+// 반복문
+// orange 찾기
+// 오렌지가 있으면 => Remove
+// 오렌지가 없으면 => 그대로
+//
